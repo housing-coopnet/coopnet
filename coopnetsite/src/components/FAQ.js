a38e57bfb7ee4f23e3cedb03df3716f832434a77
carawolfe <carawolfe@berkeley.edu>
2019-03-03 00:41:30 -0800

    get rid of bullet points

--- coopnetsite/src/components/FAQ.js
+++ coopnetsite/src/components/FAQ.js
@@ -10,6 +10,7 @@ class FAQ extends Component {
 <Row>
   <Col>
     <p>
+
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mi diam, accumsan id ante ut, interdum ullamcorper quam. Suspendisse urna turpis, tempus sed ipsum sed, porta finibus tellus. Morbi vitae consectetur mi, vel facilisis massa. Etiam bibendum vestibulum ultricies. Donec pulvinar est a odio tempor, eu suscipit enim tincidunt. Aliquam ipsum ipsum, mattis at dapibus quis, faucibus eget mi. Duis id nibh id ligula aliquam eleifend nec non risus. Etiam malesuada varius blandit. Maecenas dapibus magna ut orci auctor rhoncus. Aenean feugiat tincidunt rhoncus. Etiam molestie laoreet ligula a dapibus. Praesent eu felis eu sapien viverra rhoncus ut eu risus. Morbi pellentesque eros vel ultrices bibendum. Morbi auctor nunc ac tristique pellentesque.
     </p>
     </Col>
