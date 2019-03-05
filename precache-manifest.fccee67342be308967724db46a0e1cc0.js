3f5dccf7b01c3a5162dd946462480393eb350ce7
carawolfe <carawolfe@berkeley.edu>
2019-03-04 16:00:29 -0800

    Updates

---
+++ precache-manifest.fccee67342be308967724db46a0e1cc0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f9859b3c20f2e2296950cb92efe7b4e",
+    "url": "/coopnet/static/media/ramsay.9f9859b3.jpeg"
+  },
+  {
+    "revision": "051619303d63a29a4dd058c96f0a7cec",
+    "url": "/coopnet/static/media/jinsu.05161930.jpeg"
+  },
+  {
+    "revision": "9d4a8629b8ff29141753395da91765fd",
+    "url": "/coopnet/static/media/cara.9d4a8629.jpeg"
+  },
+  {
+    "revision": "350c1f368954f892ec4884cf4d4f8f66",
+    "url": "/coopnet/static/media/aaron.350c1f36.jpeg"
+  },
+  {
+    "revision": "e01412e18ff15237a029",
+    "url": "/coopnet/static/js/runtime~main.e01412e1.js"
+  },
+  {
+    "revision": "7dafb06ecb663850f7e0",
+    "url": "/coopnet/static/js/main.7dafb06e.chunk.js"
+  },
+  {
+    "revision": "5f3364b4c50ad2dbb9f9",
+    "url": "/coopnet/static/js/2.5f3364b4.chunk.js"
+  },
+  {
+    "revision": "7dafb06ecb663850f7e0",
+    "url": "/coopnet/static/css/main.8ddca78e.chunk.css"
+  },
+  {
+    "revision": "ff233d579f0ea5e4f1b61c425b9fb992",
+    "url": "/coopnet/index.html"
+  }
+];
